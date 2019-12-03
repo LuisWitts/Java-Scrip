ea9afa3b60adb11d8afc2f8247859b10dad25c25
LuisWitts <luis.witts@outlook.com>
2019-12-03 08:22:15 -0300

    adicionando outras modifics checkbox

--- checkbox/code.js
+++ checkbox/code.js
@@ -1,9 +1,10 @@
 var num = 0;
+var novalinha=0
 
 function novatarefa () {
     num++
         var nometarefa = document.getElementById("input").value;
-        var novalinha = '<div class="row"><div class="col-md-2"><input type="checkbox" id="check"></div><div class="col-md-8">**tituloDaTarefa**</div><div class="col-md-2">**numSequencia**</div></div>'; 
+        var novalinha = '<div class="row"><div class="col-md-2"><input type="checkbox" id="check" onclick="preencher();"></div><div class="col-md-8">**tituloDaTarefa**</div><div class="col-md-2">**numSequencia**</div></div>'; 
         novalinha = novalinha.replace("**numSequencia**",num);
         novalinha = novalinha.replace("**tituloDaTarefa**",nometarefa);
     console.log(novalinha);
@@ -13,5 +14,9 @@ document.getElementById("lista").innerHTML+=novalinha;
 
 }
 function preencher() {
+    
+    var novalinha = 'Este texto aparece riscado<div class="row"><div class="col-md-2"><input type="checkbox" id="check" onclick="preencher();"></div><div class="col-md-8" id="tarefa"><strike>**tituloDaTarefa**</strike></div><div class="col-md-2">**numSequencia**</div></div>';
+    var nometarefa = document.getElementById("input").value;
+    novalinha = novalinha.replace("**tituloDaTarefa**",nometarefa);
     
 }
